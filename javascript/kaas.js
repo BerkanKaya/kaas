22734512771539becd1ebb607086b7e17047dedf
BerkanKaya <99059255@mydavinci.nl>
2019-09-24 13:10:37 +0200

    kaas

---
+++ javascript/kaas.js
@@ -0,0 +1,68 @@
+var cheeseYellow = prompt ("Is de kaas geel?")
+
+	if(cheeseYellow == "ja"){
+	var cheeseHoles = prompt ("Zitten er gaten in?")
+	}
+
+	if(cheeseHoles == "ja"){
+	var cheeseExpensive = prompt ("Is de kaas belachelijk duur?")
+	}
+
+	if(cheeseExpensive == "ja"){
+	document.write ("Emmenthaler")
+	}
+
+	if(cheeseExpensive == "nee"){
+	document.write ("leerdammer")
+	}
+
+else if (cheeseHoles == "nee"){
+	var cheeseStone = prompt("is de kaas hard als steen?")
+}
+
+	if(cheeseStone == "ja"){
+	document.write ("Pamigiano Reggiano")
+}	
+
+	if(cheeseStone == "nee"){
+	document.write ("Goude kaas")
+}	
+
+	if(cheeseYellow == "nee"){
+	var cheeseSchimmel = prompt ("Heeft de kaas blauwe schimmels?")
+	}
+
+	if(cheeseSchimmel == "ja"){
+	var cheeseKorst1 = prompt ("Heeft de kaas een korst?")
+	}
+
+	if(cheeseKorst1 == "ja"){
+	document.write ("Bleu de Rochbaron")
+	}
+
+	if(cheeseKorst1 == "nee"){
+	document.write ("Fourme d'ambert")
+	}
+
+	if(cheeseSchimmel == "nee"){
+	var cheeseKorst2 = prompt ("Heeft de kaas een korst?")
+	}
+
+	if(cheeseKorst2 == "ja"){
+	document.write ("Camembert")
+	}
+
+	if(cheeseKorst2 == "nee"){
+	document.write ("Mozarella")
+	}
+
+
+
+
+
+
+
+
+
+	
+	
